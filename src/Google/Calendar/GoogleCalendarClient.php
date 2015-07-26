f13cfef9bd1624d66c0595fce59b8fa0dcadf885
Bernardo Pineda <_@bernardopineda.mx>
2015-07-26 16:02:51 -0500

    Added initial class structure

--- src/Google/Calendar/GoogleCalendarClient.php
+++ src/Google/Calendar/GoogleCalendarClient.php
@@ -6,9 +6,25 @@ class GoogleCalendarClient
 {
 
     private $google_client;
+
     function __construct($google_client)
     {
         $this->google_client = $google_client;
+    }
+
+    public function config()
+    {
+        
+    }
+
+    public function getCalendarEvents()
+    {
+
+    }
+
+    public function addCalendarEvent()
+    {
+
     }
 
 }
