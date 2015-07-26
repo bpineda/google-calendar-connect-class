ef775415c6625943df628e1b89b878c91c097083
Bernardo Pineda <_@bernardopineda.mx>
2015-07-26 17:22:06 -0500

    Added verification method

--- src/Google/Calendar/GoogleCalendarClient.php
+++ src/Google/Calendar/GoogleCalendarClient.php
@@ -11,6 +11,7 @@ class GoogleCalendarClient extends \Google_Client
     const CLIENT_ACCESS_TYPE = 'offline';
     private $google_client;
     private $scopes;
+    private $error_message = '';
 
     function __construct()
     {
@@ -35,6 +36,44 @@ class GoogleCalendarClient extends \Google_Client
         $this->setAccessType(self::CLIENT_ACCESS_TYPE);
 
         $credential_file = realpath(self::CREDENTIALS_PATH);
+
+        if (file_exists($credential_file)) {
+            $accessToken = file_get_contents($credential_file);
+        } else {
+            // Request authorization from the user.
+            $authUrl = $this->createAuthUrl();
+            //printf("Open the following link in your browser:\n%s\n", $authUrl);
+            $this->error_message = 'App not verified. Open the following url: ' . $authUrl;
+            return false;
+
+        }
+        $this->setAccessToken($accessToken);
+
+        // Refresh the token if it's expired.
+        if ($this->isAccessTokenExpired()) {
+            $this->refreshToken($this->getRefreshToken());
+            file_put_contents($credential_file, $this->getAccessToken());
+        }
+    }
+
+    public function getErrorMessage()
+    {
+        return $this->error_message;
+    }
+
+    public function saveCredentials($verification_code)
+    {
+        $authCode = $verification_code;
+
+        // Exchange authorization code for an access token.
+        $accessToken = $this->authenticate($authCode);
+
+        // Store the credentials to disk.
+        if(!file_exists(dirname($credential_file))) {
+            mkdir(dirname($credential_file), 0700, true);
+        }
+        file_put_contents($credential_file, $accessToken);
+        printf("Credentials saved to %s\n", $credential_file);
     }
 
     public function getCalendarEvents()
