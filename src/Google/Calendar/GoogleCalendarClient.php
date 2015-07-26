40f04a04290e530aa021a251f2f7b4aa0d482b10
Bernardo Pineda <_@bernardopineda.mx>
2015-07-26 16:25:37 -0500

    Added application constants

--- src/Google/Calendar/GoogleCalendarClient.php
+++ src/Google/Calendar/GoogleCalendarClient.php
@@ -5,11 +5,16 @@ require_once('vendor/google/apiclient/src/Google/autoload.php');
 class GoogleCalendarClient extends \Google_Client
 {
 
+    const APPLICATION_NAME = 'Google Calendar API Quickstart';
+    const CREDENTIALS_PATH = './credentials/calendar-api-quickstart.json';
+    const CLIENT_SECRET_PATH = 'client_secret.json';
     private $google_client;
+    private $scopes;
 
     function __construct()
     {
-
+        $this->google_client = new Google_Client();
+        $this->scopes = implode(' ', array( Google_Service_Calendar::CALENDAR_READONLY));
     }
 
     public function config()
