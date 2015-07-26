71ef6cefc34fca12b0241e1d64aa1d166a9ec7c1
Bernardo Pineda <_@bernardopineda.mx>
2015-07-26 15:55:05 -0500

    Extending the client class proved to be a bit complicated, so I just added a client to the constructor. I will try to extend the class later on.

--- src/Google/Calendar/GoogleCalendarClient.php
+++ src/Google/Calendar/GoogleCalendarClient.php
@@ -5,4 +5,10 @@ namespace Google\Calendar;
 class GoogleCalendarClient
 {
 
+    private $google_client;
+    function __construct($google_client)
+    {
+        $this->google_client = $google_client;
+    }
+
 }
